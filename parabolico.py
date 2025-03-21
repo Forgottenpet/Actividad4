1843c7783c2e5a25f14bb3da4133eff37a97ec58
Josue <A01665021@tec.mx>
2025-03-21 13:00:15 -0600

    Si se añade este commit. Se implementaran las funcionalidades para que el movimiento de los objetivos y de la pelota sean más rápidos. Saludos Cordiales

--- parabolico.py
+++ parabolico.py
@@ -2,48 +2,48 @@ from random import randrange
 from turtle import *
 from freegames import vector
 
-ball = vector(-200, -200)
-speed = vector(0, 0)
-targets = []
+ball = vector(-200, -200) #posicion inicial de la pelota
+speed = vector(0, 0) #velocidad inicial de la pelota roja
+targets = [] #lista para almacenar los objetivos
 
 def tap(x, y):
     "Respond to screen tap."
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 200) / 15  #aumenta la velocidad, pasando la posicion a una velocidad. Se cambio el 25 por un 15
+        speed.y = (y + 200) / 15  #aumenta la velocidad, pasando la posicion a una velocidad. Se cambio el 25 por un 15
 
-def inside(xy):
+def inside(xy):  #verifica si un punto esta dentro del rango de la pantalla
     "Return True if xy within screen."
     return -200 < xy.x < 200 and -200 < xy.y < 200
 
-def draw():
+def draw(): #funcion que dibuja los objetivos
     "Draw ball and targets."
     clear()
 
     for target in targets:
         goto(target.x, target.y)
-        dot(20, 'blue')
+        dot(20, 'blue') #dibuja los objetivos en azul
 
     if inside(ball):
         goto(ball.x, ball.y)
-        dot(6, 'red')
+        dot(6, 'red') #dibuja la pelota roja
 
     update()
 
-def move():
-    "Move ball and targets."
+def move(): 
+    "Move ball and targets." #los objetivos aparecen en pantalla 
     if randrange(40) == 0:
         y = randrange(-150, 150)
         target = vector(200, y)
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= 3.2 #reduce el eje x en 3.2 unidades. Por lo que da la sensación de ir más rápido
 
     if inside(ball):
-        speed.y -= 0.35
+        speed.y -= 0.5 #aumentamos el valor en y, para que en el mov en y sea más rápido
         ball.move(speed)
 
     dupe = targets.copy()
@@ -59,7 +59,7 @@ def move():
         if not inside(target):
             return
 
-    ontimer(move, 50)
+    ontimer(move, 35) #disminuimos el intervalo del tiempo a 35 para mayor velocidad
 
 setup(420, 420, 370, 0)
 hideturtle()
